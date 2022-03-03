62a021c90db9202b5db36a648110f0a6f986c6b4
MAS <sibahi@live.com>
2022-03-03 11:42:07 +0400

    Simple calculator app

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Calculator
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+           
+            int num2;
+            
+            int result;
+            
+            string answer;
+
+            Console.WriteLine("Hello, welcome to the calculator program!");
+
+            Console.WriteLine("Please enter your first number!");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Please enter your second number! ");
+            
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("what type of operation would you like to do?");
+            
+            Console.WriteLine("Please enter a for addition, s for subtraction, m for multiplication, and d for dividion.");
+
+            answer = Console.ReadLine();    
+        
+            if (answer == "a")
+            {
+                result = num1 + num2;
+            }
+            else if (answer == "s")
+            {
+                result = num1 - num2;
+            }    
+            else if (answer == "m")
+            {
+                result = num1 * num2;
+            }
+            else  
+            {
+                result = num1 / num2;
+            }
+             
+            Console.WriteLine("The result is " + result);
+
+            Console.WriteLine("Thank you for using the calculator program!");
+        }
+    }
+}
